ce41a36fec717c1a6623cbae05a3f9f6ecc50358
Stewpra <wilbstewart7@gmail.com>
2024-03-12 18:16:25 -0400

    added dark mode buttons on both pages

---
+++ assets/js/blog.js
@@ -0,0 +1,20 @@
+const root = document.documentElement;
+const changeColorsButton = document.getElementById("colorMode");
+let isColorModeToggled = false;
+
+changeColorsButton.addEventListener("click", function () {
+  if (isColorModeToggled) {
+    // Revert to original colors
+    root.style.setProperty("--primary", "#dee4e7");
+    root.style.setProperty("--secondary", "gray");
+    root.style.setProperty("--tertiary", "black");
+  } else {
+    // Change to new colors
+    root.style.setProperty("--primary", "#37474f");
+    root.style.setProperty("--secondary", "black");
+    root.style.setProperty("--tertiary", "#eceff1");
+  }
+
+  // Toggle the state
+  isColorModeToggled = !isColorModeToggled;
+});
